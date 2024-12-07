eb8b847fa8fa7590e13c6f1482a4d8e9c706b86f
KichdiPatel <ayanpatel09@gmail.com>
2024-12-07 12:35:35 -0500

    adding more restaurants to RestaurantPage.tsx and fixed some small styling things

--- foodie-spot/src/pages/RestaurantPage.tsx
+++ foodie-spot/src/pages/RestaurantPage.tsx
@@ -6,64 +6,57 @@ const currentRestaurants = [
     foodType: "Thai",
     location: "BU Central",
     hours: "11AM - 10PM",
-    rating: 4.3
+    rating: 4.3,
   },
   {
     restaurantName: "Victoria Seafood",
     foodType: "Chinese",
     location: "West",
     hours: "11AM - 1AM",
-    rating: 4.0
+    rating: 4.0,
   },
   {
     restaurantName: "Yard House",
     foodType: "American",
     location: "Fenway",
     hours: "11AM - 12:30AM",
-    rating: 4.3
+    rating: 4.3,
   },
   {
-    restaurantName: "Nudpob",
-    foodType: "Thai",
-    location: "BU Central",
-    hours: "11AM - 10PM",
-    rating: 4.3
-  },
-  {
-    restaurantName: "Victoria Seafood",
-    foodType: "Chinese",
+    restaurantName: "El Jefe's Taqueria",
+    foodType: "Mexican",
     location: "West",
-    hours: "11AM - 1AM",
-    rating: 4.0
+    hours: "8AM - 3AM",
+    rating: 4.5,
   },
   {
-    restaurantName: "Yard House",
-    foodType: "American",
-    location: "Fenway",
-    hours: "11AM - 12:30AM",
-    rating: 4.3
+    restaurantName: "University Grill & Pizza",
+    foodType: "Pizza",
+    location: "Central",
+    hours: "10AM - 8PM",
+    rating: 4.5,
   },
   {
-    restaurantName: "Nudpob",
-    foodType: "Thai",
-    location: "BU Central",
-    hours: "11AM - 10PM",
-    rating: 4.3
-  },
-  {
-    restaurantName: "Victoria Seafood",
+    restaurantName: "Beijing Cafe",
     foodType: "Chinese",
-    location: "West",
-    hours: "11AM - 1AM",
-    rating: 4.0
+    location: "Central",
+    hours: "11AM - 9:15PM",
+    rating: 3.5,
   },
   {
-    restaurantName: "Yard House",
-    foodType: "American",
-    location: "Fenway",
-    hours: "11AM - 12:30AM",
-    rating: 4.3
-  }
+    restaurantName: "Underdog Hot Chicken",
+    foodType: "Fried Chicken",
+    location: "Allston",
+    hours: "12PM - 12AM",
+    rating: 3.5,
+  },
+  {
+    restaurantName: "Raising Cane's",
+    foodType: "Fast Food",
+    location: "West",
+    hours: "10AM - 11PM",
+    rating: 4.0,
+  },
 ];
 
 export default function Restaurants() {
