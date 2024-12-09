453e7628ac8cc65b4639938032dd66b5a6aacbcb
Amyat103 <zhoudavid7312@gmail.com>
2024-12-08 20:32:55 -0500

    remove debug log

--- foodie-spot/src/index.d.ts
+++ foodie-spot/src/index.d.ts
@@ -1,4 +1,5 @@
 // Used this file to render image for tsx: By: Arkar Myat (David)
 // used this guide online:
 // https://dev.to/minompi/add-images-to-a-react-project-with-typescript-4gbm
+// https://stackoverflow.com/questions/51100401/typescript-image-import
 declare module '*.png';
